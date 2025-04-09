85c0ddc87d8f789c11d9ccff6a7e95667db637a4
Jeshan Akand <79209625+byte-capsule@users.noreply.github.com>
2025-04-09 07:30:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def update_live_event_info():
             if len(event["playingMetaData"])!=0 or event["contentAes128HlsUrl"]!=None:
                 stream_url=event["contentAes128HlsUrl"] if event["contentAes128HlsUrl"]!=None else event["playingMetaData"][0]["mediaUrl"]
                 try:cookie = event["playingMetaData"][0].get("signedCookie", "")
-                except:cookie=None
+                except:cookie=""
                 data={
                         "category_name":categoryname,
                         "name":name,
